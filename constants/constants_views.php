5f5603aba0a6440e0eab366226ccc443b37dc85f
DELL <DELL@10.225.183.117>
2017-12-05 15:44:36 -0600

    semaforo_index

--- constants/constants_views.php
+++ constants/constants_views.php
@@ -3,6 +3,6 @@ const VIEW_HOME ='home';
 const VIEW_ADMIN ='admin';        
 const VIEW_MONITOR ='monitoreo';
 const VIEW_TEMPLATE = 'template';
-
-
+const VIEW_LOGIN = 'login';
+const VIEW_SEMAFORO = 'semaforo_index';
 ?>
